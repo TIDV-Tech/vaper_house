60df0917f8fa1131bab5f3d5bdb2444427207115
JDRL555 <joshuleal3@gmail.com>
2023-04-21 01:08:44 -0400

    created the second page

--- client/util/data.py
+++ client/util/data.py
@@ -36,5 +36,25 @@ data = {
     {"name": "Tanques", "img": "resources/categories/tanques.jpg"},
     {"name": "e-liquids", "img": "resources/categories/e_liquids.jpg"},
     {"name": "Kits", "img": "resources/categories/kits.jpg"},
+  ],
+  "categories2": [
+    {"name": "Baterias", "img": "resources/categories2/baterias.jpg"},
+    {"name": "Mijo_Vape", "img": "resources/categories2/mijo_Vape.png"},
+    {"name": "Equipos", "img": "resources/categories2/equipos.png"},
+    {"name": "Resistencias", "img": "resources/categories2/resistencias.jpg"},
+  ],
+  "products": [
+    {"name": "Titulo", "img": "resources/products/6e575250e436fd98da66a14db8cf53f5.jpg"},
+    {"name": "Tiulo", "img": "resources/products/descarga.jpeg"},
+    {"name": "Titulo", "img": "resources/products/CBD_05_A.jpg"},
+    {"name": "Titulo", "img": "resources/products/draw_svg20210507-22909-pboesy.svg.png"},
+    {"name": "Titulo", "img": "resources/products/vaper.jpg"},
+    {"name": "Titulo", "img": "resources/products/istockphoto-1409455895-612x612.jpg"},
+    {"name": "Titulo", "img": "resources/products/hombre.jpg"},
+    {"name": "Titulo", "img": "resources/products/cannacoil-cannabis-coil-logo-vaping-260nw-1324480643.jpg"},
+    {"name": "Titulo", "img": "resources/products/Logo-Chilivaper-Final-05-300x180.png"},
+    {"name": "Titulo", "img": "resources/products/logo-1.svg"},
+    {"name": "Titulo", "img": "resources/products/cart-top-image.png"},
+    {"name": "Titulo", "img": "resources/products/vape-store-logo-design_626969-479.jpg"},
   ]
 }
--- server/database/src/controllers/purchase.controller.js
+++ server/database/src/controllers/purchase.controller.js
@@ -1,17 +1,22 @@
+import mongoose from "mongoose"
 import { Purchase } from "../models/Purchase.js"
 const purchase_controller = {}
 
-purchase_controller.registerPurchase = async (userId, productsId, purchaseData) => {
+purchase_controller.registerPurchase = async (req, res) => {
   try {
-    const { paymentMethod, totalPrice } = purchaseData
-    const purchaseSaved = await new Purchase(userId, productsId, paymentMethod, totalPrice ).save()
-    return {msg: "Purchase registered successfuly", purchaseSaved}
+    const { paymentMethod, totalPrice }   = req.body
+    let   { user, products }              = req.body
+    user        = new mongoose.Types.ObjectId(user)
+    products    = products.map(product => new mongoose.Types.ObjectId(product))
+    const data  = {user, products, paymentMethod, totalPrice}
+    const purchaseSaved = await new Purchase(data).save()
+    res.json({msg: "Purchase registered successfuly", purchaseSaved}) 
   } catch (error) {
-    return {msg: "ERROR", error: error.message}
+    res.json({msg: "ERROR", error: error.message}) 
   }
 }
 
-purchase_controller.showPurchases = async () => {
+purchase_controller.showPurchases = async (req, res) => {
   const purchases = await Purchase.aggregate([
     {
       $lookup: {
@@ -32,7 +37,8 @@ purchase_controller.showPurchases = async () => {
   ])
 
   if(!purchases.length){
-    return {msg: "There's no purchases yet!", purchases}
+    res.json({msg: "There's no purchases yet!", purchases}) 
   }
-  return {msg: "Here's the purchases!", purchases}
+  res.json({msg: "Here's the purchases!", purchases}) 
 }
+export { purchase_controller }
