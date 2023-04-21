59262fed4595d1a1fcc069b2b15e1d4f2216196c
SugeyCh <chaconsugey2003@gmail.com>
2023-04-21 11:58:18 -0400

    Commit para hacer pull

---
+++ server/api rest/src/controllers/validate_product.js
@@ -0,0 +1,42 @@
+let msg = {
+  status: false,
+  message: "Error retrieving data",
+  data: [],
+  code: 500,
+}
+
+const regisProduct = (nombre, marca, color, cantidad, fecha_ingreso, precio) => {
+	/* const product = {nombre, marca, color, cantidad, fecha_ingreso, precio}
+	if (product.value == '') {
+		msg = {
+			status: false,
+			message: "Hay un error en el registro del producto",
+			code: 500
+		}
+	} */
+
+	if(cantidad > 10){
+		msg = {
+			status: true,
+			message: "Producto ingresado a las ventas al mayor",
+			code: 200
+		}
+	} else if(cantidad <= 10) {
+		msg = {
+			status: true,
+			message: "Producto ingresado a las ventas al detal",
+			code: 200
+		}
+	} else {
+		msg = {
+			status: false,
+			message: "Hay un error en el registro del producto",
+			code: 500
+		}
+	}
+	return msg
+}
+
+module.exports = {
+	regisProduct
+}
--- server/api rest/src/server.js
+++ server/api rest/src/server.js
@@ -2,11 +2,13 @@ const express = require('express')
 const cors    = require('cors')
 const _var    = require('./global/var.js')
 const user    = require('./routes/user.routes.js')
+const product = require('./routes/product.routes.js')
 const app     = express()
 
 app.use(express.json())
 app.use(cors())
 app.use(user)
+app.use(product)
 
 app.listen(_var.PORT, (err) => {
 	if (err) throw err
