d265d129e294ed23434de68ed4729eb3ca6a2a14
Beltz Anhxton <beltz.18kyodai@gmail.com>
2023-04-21 21:50:48 -0400

    modified macros and added jinja2 structured blocks for components

--- client/util/data.py
+++ client/util/data.py
@@ -2,18 +2,28 @@ from os import *
 
 file_base = 'resources'
 logo_base = 'resources/logo'
+links     = listdir(f'static/css')
+scripts   = listdir(f'static/js')
 files     = listdir(f'static/{file_base}')
 logo      = str(listdir(f'static/{logo_base}'))
 
 data = {
   'title': 'Vaper House',
   'language': 'es',
+  'message': 'Bienvenido',
   'logo': {
-    'name': logo.split(".")[0][2:],
-    'ext': logo.split(".")[1][:-2],
+    'name': logo.split('.')[0][2:],
+    'ext': logo.split('.')[1][:-2],
     'base': f'{logo_base}/{logo[2:-2]}'
   },
-  'files': [file for file in files if '.' in file],
+  'external': [
+    'https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css'
+  ],
+  'styles': [link for link in links if '.' in link],
+  'script': [script for script in scripts if '.' in script],
+  'images':  [file for file in files if '.' in file],
+  'saldo': 15.99,
+  'user': 'Andi M',
   'warning_msg': '''Advertencia: Prohibida la venta a menores de 18 años.
     Algunos productos contienen nicotina, una sustancia
     altamente adictiva''',
@@ -30,31 +40,90 @@ data = {
     { 'name': 'Contacto', }
   ],
   'categories': [
-    {"name": "Lo más nuevo", "img": "resources/categories/lo_mas_nuevo.jfif"},
-    {"name": "Promociones", "img": "resources/categories/promociones.jfif"},
-    {"name": "Accesorios", "img": "resources/categories/accesorios.jpg"},
-    {"name": "Tanques", "img": "resources/categories/tanques.jpg"},
-    {"name": "e-liquids", "img": "resources/categories/e_liquids.jpg"},
-    {"name": "Kits", "img": "resources/categories/kits.jpg"},
+    {
+      'name': 'Lo más nuevo',
+      'img': 'resources/categories/lo_mas_nuevo.jfif'
+    },
+    {
+      'name': 'Promociones',
+      'img': 'resources/categories/promociones.jfif'
+    },
+    {
+      'name': 'Accesorios',
+      'img': 'resources/categories/accesorios.jpg'
+    },
+    {
+      'name': 'Tanques',
+      'img': 'resources/categories/tanques.jpg'
+    },
+    {
+      'name': 'e-liquids',
+      'img': 'resources/categories/e_liquids.jpg'
+    },
+    {
+      'name': 'Kits',
+      'img': 'resources/categories/kits.jpg'
+    },
   ],
-  "categories2": [
-    {"name": "Baterias", "img": "resources/categories2/baterias.jpg"},
-    {"name": "Mijo_Vape", "img": "resources/categories2/mijo_Vape.png"},
-    {"name": "Equipos", "img": "resources/categories2/equipos.png"},
-    {"name": "Resistencias", "img": "resources/categories2/resistencias.jpg"},
+  'categories2': [
+    {
+      'name': 'Baterias',
+      'img': 'resources/categories2/baterias.jpg'
+    },
+    {
+      'name': 'Mijo_Vape',
+      'img': 'resources/categories2/mijo_Vape.png'
+    },
+    {
+      'name': 'Equipos',
+      'img': 'resources/categories2/equipos.png'
+    },
+    {
+      'name': 'Resistencias',
+      'img': 'resources/categories2/resistencias.jpg'
+    },
   ],
-  "products": [
-    {"name": "Titulo", "img": "resources/products/6e575250e436fd98da66a14db8cf53f5.jpg"},
-    {"name": "Tiulo", "img": "resources/products/descarga.jpeg"},
-    {"name": "Titulo", "img": "resources/products/CBD_05_A.jpg"},
-    {"name": "Titulo", "img": "resources/products/draw_svg20210507-22909-pboesy.svg.png"},
-    {"name": "Titulo", "img": "resources/products/vaper.jpg"},
-    {"name": "Titulo", "img": "resources/products/istockphoto-1409455895-612x612.jpg"},
-    {"name": "Titulo", "img": "resources/products/hombre.jpg"},
-    {"name": "Titulo", "img": "resources/products/cannacoil-cannabis-coil-logo-vaping-260nw-1324480643.jpg"},
-    {"name": "Titulo", "img": "resources/products/Logo-Chilivaper-Final-05-300x180.png"},
-    {"name": "Titulo", "img": "resources/products/logo-1.svg"},
-    {"name": "Titulo", "img": "resources/products/cart-top-image.png"},
-    {"name": "Titulo", "img": "resources/products/vape-store-logo-design_626969-479.jpg"},
-  ]
+  'products': [
+    {
+      'name': 'Titulo',
+      'img': 'resources/products/6e575250e436fd98da66a14db8cf53f5.jpg',
+      'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias commodi temporibus provident porro itaque saepe, quo repellat quod.',
+      'price': 10.00
+    },
+    {
+      'name': 'Tiulo',
+      'img': 'resources/products/descarga.jpeg',
+      'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias commodi temporibus provident porro itaque saepe, quo repellat quod.',
+      'price': 10.00
+    },
+    {
+      'name': 'Titulo',
+      'img': 'resources/products/CBD_05_A.jpg',
+      'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias commodi temporibus provident porro itaque saepe, quo repellat quod.',
+      'price': 10.00
+    },
+    {
+      'name': 'Titulo',
+      'img': 'resources/products/draw_svg20210507-22909-pboesy.svg.png',
+      'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias commodi temporibus provident porro itaque saepe, quo repellat quod.',
+      'price': 10.00
+    },
+  ],
+  'info': ['¿Quiénes somos?', '¿Qué ofrecemos?'],
+  'politica': ['Términos y condiciones', 'Políticas de seguridad', 'Políticas de Cookies'],
+  'social': [
+    {
+      'red': 'facebook',
+      'ico': 'bx bxl-instagram bx-sm',
+      'dir': 'vaperhouse.vzla'
+    },
+    {
+      'red': 'instagram',
+      'ico': 'bx bxl-facebook-circle bx-sm',
+      'dir': 'Vaper House Café'
+    },
+  ],
+  'direccion': 'Barrio Obrero carrera 20 entre calles 13 y 14, diagonal al Cine Pirineos, frente a cada Matina',
+  'contacto': 'comprasvaperhouse@gmail.com',
+  'numeros': ['+58412-1729864', '+58414-7573307']
 }
